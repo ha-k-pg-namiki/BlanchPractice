18c1857c1a7580066af3b7b2bd120cc8715253ca
MSI\nai14 <nai1415lqego@gmail.com>
2020-09-01 11:21:40 +0900

    HelloWorldを表示する関数の追加

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,13 +1,21 @@
 ﻿#include <stdlib.h>
 #include <stdio.h>
 
+void DrawHelloWorld();
+
+
 
 int main()
 {
-	printf("Hello World\n");
+	DrawHelloWorld();
 
 	system("pause");
 	return 0;
 }
 
 
+//Hello Worldを表示する関数
+void DrawHelloWorld()
+{
+	printf("Hello World\n");
+}
